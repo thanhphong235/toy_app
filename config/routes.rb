ab45af413adfb8300178dc7e9e75f27d0d703af9
nguyễn phong <thanhphong23502@gmail.com>
2025-10-10 17:39:30 +0700

    chuong5-1

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   # root "posts#index"
   # root 'users#index'
   # root 'application#hello'
-  root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
+  # root 'static_pages#home'
+  # get '/help', to: 'static_pages#help'
+  # get '/about', to: 'static_pages#about'
+root 'static_pages#home'
+get '/help', to: 'static_pages#help'
+get '/about', to: 'static_pages#about'
+get '/contact', to: 'static_pages#contact'
 end
