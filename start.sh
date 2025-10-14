fa728456606746b723f13485a79ce97fde0920ac
nguyễn phong <thanhphong23502@gmail.com>
2025-10-14 15:09:16 +0700

    Fix PORT handling in start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,8 @@ bundle exec rake db:migrate
 # 2. Precompile assets (nếu chưa precompile)
 bundle exec rake assets:precompile
 
-# 3. Start Rails server trên cổng Render
+# 3. Đặt PORT nếu chưa có
+export PORT=${PORT:-3000}
+
+# 4. Start Rails server
 bundle exec rails server -b 0.0.0.0 -p $PORT
