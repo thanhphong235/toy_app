27a82dcfa11276a6744d27f640a050b9ff585e94
nguyễn phong <thanhphong23502@gmail.com>
2025-10-16 14:16:05 +0700

    abc

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,43 +1,70 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :set_micropost, only: %i[ show edit update destroy ]
   before_action :correct_user, only: :destroy
 
+  # GET /microposts or /microposts.json
+  def index
+    @microposts = Micropost.all
+  end
+
+  # GET /microposts/1 or /microposts/1.json
+  def show
+  end
+
+  # GET /microposts/new
+  def new
+    @micropost = Micropost.new
+  end
+
+  # GET /microposts/1/edit
+  def edit
+  end
+
+  # POST /microposts or /microposts.json
   def create
     @micropost = current_user.microposts.build(micropost_params)
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
-      render 'static_pages/home', status: :unprocessable_entity
+      render 'static_pages/home'
     end
   end
 
+  # PATCH/PUT /microposts/1 or /microposts/1.json
+  def update
+    respond_to do |format|
+      if @micropost.update(micropost_params)
+        format.html { redirect_to @micropost, notice: "Micropost was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @micropost }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  # DELETE /microposts/1 or /microposts/1.json
   def destroy
-    @micropost.destroy
+    @micropost.destroy!
     flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_url, status: :see_other
+    redirect_to request.referrer || root_url
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_micropost
+      @micropost = Micropost.find(params[:id])
+    end
 
+    # Only allow a list of trusted parameters through.
     def micropost_params
       params.require(:micropost).permit(:content, :image)
     end
-
-    # Chỉ cho phép người dùng đăng nhập
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
-
-    # Đảm bảo chỉ xóa micropost của chính mình
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url, status: :see_other if @micropost.nil?
+      redirect_to root_url if @micropost.nil?
     end
 end
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,54 +1,50 @@
-require "test_helper"
-
+require 'test_helper'
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-
   test "micropost interface" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    assert_select 'input[type=file]'
-
-    # Gửi micropost không hợp lệ
+    assert_select 'input[type=FILL_IN]'
+    # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
-
-    # Gửi micropost hợp lệ
+    assert_select 'a[href=?]', '/?page=2' # Correct pagination link
+    # Valid submission
     content = "This micropost really ties the room together"
+    image = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpeg')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost:
+                                    { content: content, image: image } }
     end
-    assert_redirected_to root_url
+    assert FILL_IN.image.attached?
     follow_redirect!
     assert_match content, response.body
-
-    # Xóa micropost
+    # Delete post
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
-
-    # Truy cập user khác (không có nút xóa)
+    # Visit different user (no delete links)
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
-
   test "micropost sidebar count" do
     log_in_as(@user)
     get root_path
-    assert_match "#{@user.microposts.count} microposts", response.body
-    # user chưa có micropost
+    assert_match "#{FILL_IN} microposts", response.body
+    # User with zero microposts
     other_user = users(:malory)
     log_in_as(other_user)
     get root_path
     assert_match "0 microposts", response.body
     other_user.microposts.create!(content: "A micropost")
     get root_path
-    assert_match "1 micropost", response.body
+    assert_match FILL_IN, response.body
   end
 end
