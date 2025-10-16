c80e015e6ceea0742a31254a73aedb35ced5eef6
nguyễn phong <thanhphong23502@gmail.com>
2025-10-16 15:48:18 +0700

    a

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,6 +10,6 @@ class Micropost < ApplicationRecord
                                     message: "should be less than 5MB" }
   # Returns a resized image for display.
   def display_image
-    image.variant(resize_to_limit: [500, 500]) if image.attached?
+    image.variant(resize_to_limit: [500, 500])
   end
 end
