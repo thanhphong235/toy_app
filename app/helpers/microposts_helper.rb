d1787278dfc6c86e3f04dc3b636e074cfbcc2c5a
nguyễn phong <thanhphong23502@gmail.com>
2025-10-16 16:01:26 +0700

    a

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,8 @@
 module MicropostsHelper
+    # Trả về variant đã resize của ảnh Micropost
+  def display_image(micropost, width: 500, height: 500)
+    return unless micropost.image.attached?
+
+    micropost.image.variant(resize_to_limit: [width, height])
+  end
 end
